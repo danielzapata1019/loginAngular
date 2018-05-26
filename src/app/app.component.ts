f9b2d30eef53216a29f7c981934597198eb6f984
Daniel Zapata <danielzapata1019@gmail.com>
2018-05-26 10:18:07 -0500

    Antes de estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { Router } from '@angular/router'; //Siempre que vaya a llamar una ruta de mi código debo implementar la clase router
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:'<router-outlet></router-outlet>'
+  /*templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']*/
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private router:Router){
+    this.router.navigate(['login']);//Enrutar al login
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouteRoutingModule } from './route/route-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouteRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
